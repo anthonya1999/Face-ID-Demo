ad1d12f47e0ed4c23825cf60a6829495df7d82cc
Anthony Agatiello <anthonya1999@gmail.com>
2017-09-27 15:47:27 -0400

    Fixed crashes on iOS 11.1

--- Face ID/ViewController.m
+++ Face ID/ViewController.m
@@ -14,9 +14,6 @@
 @implementation ViewController
 
 - (IBAction)showFaceID:(UIButton *)button {
-    BOOL (*objc_msgSendBool)(id self, SEL _cmd) = dlsym(RTLD_DEFAULT, "objc_msgSend");
-    NSParameterAssert(objc_msgSendBool);
-    
     void *PreferencesUI = dlopen("/System/Library/PrivateFrameworks/PreferencesUI.framework/PreferencesUI", RTLD_LAZY);
     NSParameterAssert(PreferencesUI);
     
@@ -27,16 +24,7 @@
     UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(dismissController)];
     [settingsPearlVC.navigationItem setRightBarButtonItem:doneButton];
     
-    if (objc_msgSendBool(settingsPearlVC, sel_getUid("isEnrolled")) == NO) {
-        // Make Face ID always enrolled (just always return true)
-        if ([settingsPearlVC respondsToSelector:sel_getUid("isEnrolled")]) {
-            NSLog(@"Face ID is not enrolled...overriding!");
-            Method isEnrolled_original = class_getInstanceMethod([settingsPearlVC class], sel_getUid("isEnrolled"));
-            NSParameterAssert(isEnrolled_original);
-            Method isEnrolled_override = class_getInstanceMethod([self class], @selector(isEnrolled_override));
-            NSParameterAssert(isEnrolled_override);
-            method_exchangeImplementations(isEnrolled_original, isEnrolled_override);
-        }
+    if (!_displayedControllerOnce) {
         AVCaptureVideoPreviewLayer *videoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] init];
         if ([videoPreviewLayer respondsToSelector:sel_getUid("setVideoPreviewFilters:")]) {
             NSLog(@"Overriding setVideoPreviewFilters:");
@@ -46,32 +34,18 @@
             NSParameterAssert(setVideoPreviewFilters_override);
             method_exchangeImplementations(setVideoPreviewFilters_original, setVideoPreviewFilters_override);
         }
-        if ([settingsPearlVC respondsToSelector:sel_getUid("numberOfAppsUsingPearl")]) {
-            NSLog(@"Overriding numberOfAppsUsingPearl");
-            Method numberOfAppsUsingPearl_original = class_getInstanceMethod([settingsPearlVC class], sel_getUid("numberOfAppsUsingPearl"));
-            NSParameterAssert(numberOfAppsUsingPearl_original);
-            Method numberOfAppsUsingPearl_override = class_getInstanceMethod([self class], @selector(numberOfAppsUsingPearl_override));
-            NSParameterAssert(numberOfAppsUsingPearl_override);
-            method_exchangeImplementations(numberOfAppsUsingPearl_original, numberOfAppsUsingPearl_override);
-        }
     }
     
     UINavigationController *navVC = [[UINavigationController alloc] initWithRootViewController:settingsPearlVC];
-    [self presentViewController:navVC animated:YES completion:nil];
+    [self presentViewController:navVC animated:YES completion:^{
+        _displayedControllerOnce = YES;
+    }];
     
     dlclose(PreferencesUI);
 }
 
 - (void)dismissController {
     [self dismissViewControllerAnimated:YES completion:nil];
-}
-
-- (BOOL)isEnrolled_override {
-    return YES;
-}
-
-- (unsigned long long)numberOfAppsUsingPearl_override {
-    return 5;
 }
 
 - (void)setVideoPreviewFilters_override {
